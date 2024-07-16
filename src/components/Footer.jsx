2ed89bce5ec7dab6e3e3ef9a743ab97a4bf724e4
Lee <leaveil.johnson1881@gmail.com>
2024-07-16 21:10:34 +0200

    Added new feature

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <a href="#hero"><img src={logo} alt="Logo" className="logo" /></a>
-      <p>&copy; 2024 1001 Whiskeys. All rights reserved.</p>
+      <p>&copy; 2024 1001 Whiskies. All rights reserved.</p>
     </footer>
   );
 };
--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Members.css';
-import Navbar from '../components/Navbar'; 
-import Footer from '../components/Footer'; 
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 import member1 from '../assets/members/faadiel.jpeg';
 import member2 from '../assets/members/faadiel.jpeg';
 import member3 from '../assets/members/faadiel.jpeg';
@@ -31,22 +31,39 @@ const members = [
 ];
 
 const Members = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(null);
+
+  const openImage = (index) => setCurrentImageIndex(index);
+  const closeImage = () => setCurrentImageIndex(null);
+  const showNextImage = () => setCurrentImageIndex((index) => (index + 1) % members.length);
+  const showPreviousImage = () => setCurrentImageIndex((index) => (index - 1 + members.length) % members.length);
+
   return (
     <div>
-      <Navbar /> 
+      <Navbar />
       <div className="members">
         <h1>The Whiskey Connoisseurs</h1>
         <p className='member-description'>A close-knit community of whiskey enthusiasts dedicated to sharing knowledge, enjoying tastings, and celebrating the rich heritage of whiskey. Our members relish in curated experiences and camaraderie centered around their passion for fine whiskies.</p>
         <div className="members-grid">
           {members.map((member, index) => (
-            <div className="member-card" key={index}>
+            <div className="member-card" key={index} onClick={() => openImage(index)}>
               <img src={member.image} alt={member.name} />
               <p>{member.name}</p>
             </div>
           ))}
         </div>
       </div>
-      <Footer /> 
+      {currentImageIndex !== null && (
+        <div className="lightbox" onClick={closeImage}>
+          <div className="lightbox-content" onClick={(e) => e.stopPropagation()}>
+            <img src={members[currentImageIndex].image} alt={members[currentImageIndex].name} />
+            <button className="close-button" onClick={closeImage}>&times;</button>
+            <button className="arrow left-arrow" onClick={showPreviousImage}>&larr;</button>
+            <button className="arrow right-arrow" onClick={showNextImage}>&rarr;</button>
+          </div>
+        </div>
+      )}
+      <Footer />
     </div>
   );
 };
